07f512e8c2c57f28c1edeb1f401b41bb59044e17
Luis Daniel Unapanta <ldurider@gmail.com>
2018-07-26 22:15:03 -0500

    estilo login

--- BlackCatApp/src/app/login/login.component.ts
+++ BlackCatApp/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {ServicioAlcoholimetroService} from "../servicio-alcoholimetro/servicio-alcoholimetro.service";
-import {Router} from "@angular/router";
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
 import {AuthService} from "../servicios/auth.service";
 import {HttpClient} from "@angular/common/http";
 
@@ -11,6 +13,11 @@ import {HttpClient} from "@angular/common/http";
 })
 export class LoginComponent implements OnInit {
 
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+
   correo1: string;
   password1: string;
   estado: boolean;
@@ -18,14 +25,20 @@ export class LoginComponent implements OnInit {
   invitacionesUsuario: any = []
 
 
-  constructor(private http: HttpClient,private _auth: AuthService, private router:Router, private _servicio: ServicioAlcoholimetroService) {
+  constructor(private http: HttpClient,private _auth: AuthService, private router:Router,private formBuilder: FormBuilder,
+              private route: ActivatedRoute,) {
     this.estado = false;
 
   }
 
   ngOnInit() {
-
+    this.loginForm = this.formBuilder.group({
+      correo: ['',  Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.required]
+    });
   }
+
+  get f() { return this.loginForm.controls; }
 
   ingresoUsuario(event, formData) {
     //console.log(event);
@@ -37,6 +50,21 @@ export class LoginComponent implements OnInit {
     const password = target.querySelector('#password').value
 
     this._auth.consultarUsuario(correo, password);
+
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this._auth.consultarUsuario(this.loginForm.value.correo, this.loginForm.value.password);
+
+
+    //this.loading = true;
 
   }
 
